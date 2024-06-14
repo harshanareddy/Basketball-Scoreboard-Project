77d6df83ffaddac659d351d79d9a2a0a726566f9
harshanareddy <106921849+harshanareddy@users.noreply.github.com>
2024-06-14 01:17:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,29 +6,57 @@ let guestCount = 0;
 function plusHomeOne(){
     homeCount += 1;
     homePointsEl.textContent = homeCount;
+    checkLead();
 }
 
 function plusHomeTwo(){
     homeCount += 2;
     homePointsEl.textContent = homeCount;
+    checkLead();
 }
 
 function plusHomeThree(){
     homeCount += 3;
     homePointsEl.textContent = homeCount;
+    checkLead();
 }
 
 function plusGuestOne(){
     guestCount += 1;
     guestPointsEl.textContent = guestCount;
+    checkLead();
 }
 
 function plusGuestTwo(){
     guestCount += 2;
     guestPointsEl.textContent = guestCount;
+    checkLead();
 }
 
 function plusGuestThree(){
     guestCount += 3;
     guestPointsEl.textContent = guestCount;
+    checkLead();
+}
+
+function reset(){
+    homePointsEl.textContent = 0;
+    guestPointsEl.textContent = 0;
+    homeCount = 0;
+    guestCount = 0;
+    homePointsEl.classList.remove("lead");
+    guestPointsEl.classList.remove("lead");
+}
+
+function checkLead(){
+    if(homeCount > guestCount){
+        homePointsEl.classList.add("lead");
+        guestPointsEl.classList.remove("lead");
+    }else if(guestCount > homeCount){
+        guestPointsEl.classList.add("lead");
+        homePointsEl.classList.remove("lead");
+    }else{
+        guestPointsEl.classList.remove("lead");
+        homePointsEl.classList.remove("lead");
+    }
 }
